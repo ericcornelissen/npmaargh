eea1f612e692b42e6252363ca9bf64b5d614e992
Eric Cornelissen <ericornelissen@gmail.com>
2026-07-25 10:49:50 +0200

    Fix compatibility issue with npm v12

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-/// Copyright (C) 2026  Eric Cornelissen
-///
-/// This program is free software: you can redistribute it and/or modify
-/// it under the terms of the GNU Affero General Public License as published by
-/// the Free Software Foundation, either version 3 of the License, or
-/// (at your option) any later version.
-///
-/// This program is distributed in the hope that it will be useful,
-/// but WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-/// GNU Affero General Public License for more details.
-///
-/// You should have received a copy of the GNU Affero General Public License
-/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//! Copyright (C) 2026  Eric Cornelissen
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU Affero General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU Affero General Public License for more details.
+//!
+//! You should have received a copy of the GNU Affero General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import { exec as _exec } from "node:child_process";
 import { readFile } from "node:fs/promises";
@@ -112,7 +112,8 @@ async function npmView(name, version) {
 				return manifest;
 			} else {
 				const { stdout } = await exec(`npm view ${subject} --json`, { cwd: wd });
-				return JSON.parse(stdout);
+				const view = JSON.parse(stdout);
+				return Array.isArray(view) ? view[0] : view;
 			}
 		}
 
